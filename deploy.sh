1934e3cfe7d37a118b45b3cf9ad8af0a59bb781b
johnko <git@johnko.ca>
2015-04-25 23:11:31 -0400

    change order: upload doc then push app

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ DBNAME=example
 DBURL=http://localhost:5984/${DBNAME}
 
 kanso install
-kanso push ${DBURL}
+
 kanso transform add-ids data/example.json data/example-with-ids.json
 kanso upload data/example-with-ids.json ${DBURL}
+
+kanso push ${DBURL}
