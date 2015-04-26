b0bd94202335cbd9edae8fec2ca8d42b1650213d
johnko <git@johnko.ca>
2015-04-25 23:06:27 -0400

    variables!

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
+DBNAME=example
+DBURL=http://localhost:5984/${DBNAME}
+
 kanso install
-kanso push http://localhost:5984/example
+kanso push ${DBURL}
 kanso transform add-ids data/example.json data/example-with-ids.json
-kanso upload data/example-with-ids.json http://localhost:5984/example
+kanso upload data/example-with-ids.json ${DBURL}
